79b1c28c536ddf37e4d03b478b0da8117ab88e11
mariaa <127808938+mariaoktadian@users.noreply.github.com>
2024-07-01 12:16:48 +0800

    Add files via upload

---
+++ mariaUas/projectUAS/src/projectuas/ProjectUAS.java
@@ -0,0 +1,18 @@
+
+package projectuas;
+
+/**
+ *
+ * @author mariaa
+ * tgl: 01-07-2024
+ */
+public class ProjectUAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
